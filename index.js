b0a2b3f996d8cc979cc1362ac572f55ba677f1a2
jayanthbr29 <jayanthbr@digi9.co.in>
2025-01-15 11:56:14 +0530

    setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/dbConfig");
 require("dotenv").config();
+const {Webhook}= require('svix');
 
 const app = express();
 
@@ -13,7 +14,7 @@ connectDB()
 
 
     // Start the Server
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
@@ -21,4 +22,14 @@ connectDB()
   .catch((err) => {
     console.error("MongoDB connection error:", err);
     process.exit(1); // Exit process with failure
+  });
+
+
+  const userRoutes = require("./src/routes/userRoutes");
+
+  app.use("/users", userRoutes);
+
+
+  app.listen(5000, () => {
+    console.log("Server started on port 5000");
   });
