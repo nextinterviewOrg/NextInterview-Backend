e218491a8f66382c44d1467755ba312c047fdf37
jayanthbr29 <jayanthbr@digi9.co.in>
2025-02-15 15:02:15 +0530

    user sesssion api

--- src/controllers/chatGptController.js
+++ src/controllers/chatGptController.js
@@ -4,13 +4,14 @@ const API_KEY =process.env.GPT_API_KEY
  
 const API_URL = "https://api.openai.com/v1/chat/completions";
 
-exports.getChatGPTResponse = async (message) => {
+exports.getChatGPTResponse = async (req,res) => {
   try {
+    console.log("api Called",req.body)
     const response = await axios.post(
       API_URL,
       {
         model: "gpt-4", // or "gpt-3.5-turbo"
-        messages: [{ role: "user", content: message }],
+        messages: [{ role: "user", content: req.body.message }],
         temperature: 0.7,
       },
       {
@@ -20,7 +21,8 @@ exports.getChatGPTResponse = async (message) => {
         },
       }
     );
-    return response.data.choices[0].message.content;
+    // return response.data.choices[0].message.content;
+   return res.status(200).json({message:"success",data:response.data.choices[0].message.content});
   } catch (error) {
     console.error("Error fetching ChatGPT response:", error);
     return "Something went wrong.";
