e9950066b32a0bae506ac36ce44f2c9902d20873
jayanthbr29 <jayanthbr@digi9.co.in>
2025-03-10 18:05:40 +0530

    user progress and module progress

--- src/routes/userProgressRoutes.js
+++ src/routes/userProgressRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { startModule, startTopic, startSubtopic, completeTopic, completeModule, completeSubtopic, getUserProgress, getProgressStats } = require("../controllers/userProgressController");
+const { startModule, startTopic, startSubtopic, completeTopic, completeModule, completeSubtopic, getUserProgress, getProgressStats, getUserProgressByModuleCode, getUserProgressBySubtopicCode, getUserProgressByTopicCode } = require("../controllers/userProgressController");
 
 
 const router = express.Router();
@@ -15,5 +15,7 @@ router.post("/completeTopic",completeTopic);
 router.post("/completeSubTopic",completeSubtopic);
 router.get("/getProgress/:userId",getUserProgress);
 router.get("/getProgressStats/:userId",getProgressStats);
-
+router.post("/getprogressByUserModule",getUserProgressByModuleCode);
+router.post("/getprogressByUserTopic",getUserProgressByTopicCode);
+router.post("/getprogressByUserSubtopic",getUserProgressBySubtopicCode);
 module.exports = router;
