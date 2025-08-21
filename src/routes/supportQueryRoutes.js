a0d42b77ccea9dee7a3de5a573d8b5e8a2573526
Roshni <roshni@digi9.co.in>
2025-08-22 00:14:21 +0530

    support query backend

--- src/routes/supportQueryRoutes.js
+++ src/routes/supportQueryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require("../middleware/authorizeRole");
 const supportQueryController = require("../controllers/supportQueryController");
 
 
@@ -9,5 +9,15 @@ router.get("/:id", supportQueryController.getSupportQueryById);
 router.get("/getQuery/all", supportQueryController.getAllSupportQuery);
 router.put("/updatequery/:id", supportQueryController.updateSupportQuery);
 router.get("/getSupportStatistics/stats", supportQueryController.getSupportQueryStats);
-router.post('/support/:id/admin-message', supportQueryController.addAdminMessageToSupportQuery);
+
+router.post("/:queryId/admin-message", supportQueryController.sendMessageFromAdmin);
+
+// Send message from user to admin
+router.get("/:queryId/user-message", auth, supportQueryController.sendMessageFromUser);
+
+// Get chat log for a specific query
+router.get("/:queryId/chatlog", supportQueryController.getChatLog);
+
+// Get support queries by user ID
+router.get("/user/:userId", supportQueryController.getSupportQueriesByUser);
 module.exports = router;
